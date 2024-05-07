7eb16c8c1f80c1a11b21e47b6ee389ea23ef87fb
Alex Kearns <alex@alexkearns.co.uk>
2024-05-07 21:03:42 +0100

    Upgrade instance size to t3.xlarge

--- example_application/deploy.sh
+++ example_application/deploy.sh
@@ -2,4 +2,4 @@ AWS_REGION=us-east-1 \
 aws cloudformation deploy \
   --template-file ./template.yaml \
   --stack-name example-app-for-finops-bedrock-demo-ABC \
-  --parameter-overrides InstanceType=t3.micro
+  --parameter-overrides InstanceType=t3.xlarge
